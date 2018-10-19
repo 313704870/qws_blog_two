876af5d3a736157c3e5eb2eb1df5e3a16d62edab
秦文帅 <313704870@qq.com>
2018-10-19 14:53:51 +0800

    优化上图图片检测方法

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -10,14 +10,6 @@ class ImageUploadHandler
 
     public function save($file, $folder, $file_prefix,$max_width = false)
     {
-        // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
-        // 文件夹切割能让查找效率更高。
-        $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
-
-        // 文件具体存储的物理路径，`public_path()` 获取的是 `public` 文件夹的物理路径。
-        // 值如：/home/vagrant/Code/larabbs/public/uploads/images/avatars/201709/21/
-        $upload_path = public_path() . '/' . $folder_name;
-
         // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
         $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
@@ -30,6 +22,14 @@ class ImageUploadHandler
             return false;
         }
 
+        // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
+        // 文件夹切割能让查找效率更高。
+        $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
+
+        // 文件具体存储的物理路径，`public_path()` 获取的是 `public` 文件夹的物理路径。
+        // 值如：/home/vagrant/Code/larabbs/public/uploads/images/avatars/201709/21/
+        $upload_path = public_path() . '/' . $folder_name;
+        
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path, $filename);
 
