b74f68764f1a02bbb7573e0fd8d55e25613e28ed
秦文帅 <313704870@qq.com>
2018-10-19 14:48:26 +0800

    用户可以上传头像了

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Models\User;
-
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
@@ -33,7 +32,7 @@ class UsersController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -44,7 +43,7 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
     }
 
     public function edit(User $user)
@@ -53,10 +52,17 @@ class UsersController extends Controller
     }
 
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
     {
-//        dd($request->all());
-        $user->update($request->all());
+        $data = $request->all();
+        //dd($request->all());
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar,'avatars',$user->id,362);
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -24,20 +24,29 @@ class UserRequest extends FormRequest
      */
     public function rules()
     {
+        /**
+         * unique:table,column,except,idColumn
+         * 在 table 数据表里检查 column ，除了 idColumn 为 except 的数据。
+         * 意思就是检查本条数据之外的数据，看本次修改的用户名是否有重名的。有重名的不让修改成功
+         */
         return [
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 200px 以上',
             'name.unique' => '用户名已被占用，请重新填写',
             'name.regex' => '用户名只支持英文、数字、横杆和下划线。',
             'name.between' => '用户名必须介于 3 - 25 个字符之间。',
             'name.required' => '用户名不能为空。',
+            'introduction.max' => '个人介绍不能超过80个字符'
         ];
     }
 }
