a70408e6582c807ab1952f2c25214c7df8e383f8
秦文帅 <313704870@qq.com>
2018-10-23 07:53:48 +0000

    创建帖子

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,5 +3,12 @@
 //处理css 路由的
 function route_class()
 {
+    //echo(Route::currentRouteName()); 当前路由。
     return str_replace('.', '-', Route::currentRouteName());
+}
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
 }
